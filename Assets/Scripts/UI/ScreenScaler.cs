f81baf2dc9b5810405cf076007165a5710268951
Matthew Bower <mbower386>
2016-08-07 03:55:03 -0700

    Created item slot prefab.  Still need to fix UI size issues across various devices.

---
+++ Assets/Scripts/UI/ScreenScaler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScreenScaler : MonoBehaviour 
+{
+    CanvasScaler screenScaler;
+    float screenDpi;
+
+	// Use this for initialization
+	void Start () 
+    {
+        screenScaler = gameObject.GetComponent<CanvasScaler>();
+
+        screenDpi = Screen.dpi;
+
+        if (screenDpi > -1)
+        {
+            screenScaler.defaultSpriteDPI = screenDpi;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour 
+{
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DisableBoolAnimator (Animator anim)
+    {
+        anim.SetBool("IsDisplayed", false);
+
+    }
+
+    public void EnableBoolAnimator (Animator anim)
+    {
+        anim.SetBool("IsDisplayed", true);
+        GUI.color = Color.black;
+    }
+
+    public void NavigateTo (int scene)
+    {
+        Application.LoadLevel(scene);
+    }
+
+
+}
