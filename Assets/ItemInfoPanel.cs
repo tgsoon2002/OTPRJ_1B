2846b5e329b107db333dc7c78cccdb492c007ae0
JChan2787 <jaychan027@gmail.com>
2016-08-16 22:17:27 -0700

    WIP 2

---
+++ Assets/ItemInfoPanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemInfoPanel : MonoBehaviour 
+{
+    public GameObject Item_Info_Panel;
+    private bool isShowing;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {/*
+        if (Input.GetButton("
+       */
+   	}
+}
